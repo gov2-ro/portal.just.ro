98b435204f5f490d6b037ea14915e6a0d6396db6
pax <pax@mioritics.ro>
2021-10-31 23:48:57 +0200

    init

---
+++ createDb.py
@@ -0,0 +1,96 @@
+import sqlite3
+
+dbfile = "data/portal-just.db"
+
+conn = sqlite3.connect(dbfile)
+c = conn.cursor()
+""" 
+Dosar
+DosarParte
+DosarSedinta
+DosarCaleAtac
+Sedinta
+SedintaDosar
+ """
+
+c.execute(
+    """
+          CREATE TABLE "Dosar" (
+            "numar"	TEXT NOT NULL UNIQUE,
+            "Numar vechi"	TEXT,
+            "data"	TEXT,
+            "institutie"	TEXT,
+            "categorieCaz"	TEXT,
+            "stadiuProcesual"	TEXT,
+            "parti"	TEXT,
+            "sedinte"	TEXT,
+            "caiAtac"	TEXT
+          )
+          """
+)
+c.execute(
+    """
+          CREATE TABLE "DosarParte" (
+            "xnumardosar"  TEXT NOT NULL,
+            "nume"	TEXT,
+            "calitateParte"	TEXT
+          )
+          """
+)
+c.execute(
+    """
+          CREATE TABLE "DosarSedinta" (
+            "xnumardosar"  TEXT NOT NULL,
+            "complet" TEXT,
+            "data" TEXT,
+            "ora" TEXT,
+            "soluţie" TEXT,
+            "soluţieSumar" TEXT,
+            "dataPronuntare" TEXT,
+            "documentSedinta" TEXT,
+            "numarDocument" TEXT,
+            "dataDocument" TEXT
+          )
+          """
+)
+
+c.execute(
+    """
+          CREATE TABLE "DosarCaleAtac" (
+            "xnumardosar"  TEXT NOT NULL,
+            "dataDeclarare"	TEXT,
+            "parteDeclaratoare"	TEXT,
+            "tipCaleAtac"	TEXT
+          )
+          """
+)
+
+c.execute(
+    """
+          CREATE TABLE "Sedinta" (
+            "xnrsedinta"  ID PRIMARY KEY,
+            "departament"	TEXT,
+            "complet"	TEXT,
+            "data"	TEXT,
+            "ora"	TEXT,
+            "dosare"	TEXT
+          )
+          """
+)
+c.execute(
+    """
+          CREATE TABLE "SedintaDosar" (
+            "xnrsedinta"  INT NOT NULL,
+            "numar" TEXT NOT NULL UNIQUE,
+            "numar vechi" TEXT,
+            "data" TEXT,
+            "ora" TEXT,
+            "categorieCaz" TEXT,
+            "stadiuProcesual" TEXT
+          )
+          """
+)
+
+conn.commit()
+
+print('created ' + dbfile)
