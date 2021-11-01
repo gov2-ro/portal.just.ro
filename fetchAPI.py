fe7f395cf0fc44bbd3202db9211b38c985949f16
pax <pax@mioritics.ro>
2021-11-01 15:05:52 +0200

    feth api by days prototype

---
+++ fetchAPI.py
@@ -0,0 +1,55 @@
+import requests
+import gzip
+
+dir_responsexml = 'data/cached-responses/xml/'
+dir_responsexmlgz = 'data/cached-responses/xmlgz/'
+url="http://portalquery.just.ro/Query.asmx?WSDL"
+#headers = {'content-type': 'application/soap+xml'}
+headers = {'content-type': 'text/xml'}
+#TODO: loop or add all Instanțe or just ignore
+
+# cautaDosar <date:yyyy-mm-dd mandatory> <days=1> <direction=back> 
+def cautaDosarXMLQ(date, *opts):
+  if not opts:
+    dateStart = date + 'T00:00:00Z'
+    dateEnd = date + 'T23:59:59Z'
+  # TODO: check direction
+  else:
+    dateStart = date + 'T00:00:00Z'
+    # TODO: smartly traverse dates 31 + 1 = 1 m+1 
+
+  qbody = """<?xml version="1.0" encoding="utf-8"?>
+  <soap12:Envelope xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+    <soap12:Body>
+      <CautareDosare xmlns="portalquery.just.ro">
+        <dataStart>{dataStart}</dataStart>
+        <dataStop>{dataStop}</dataStop>
+      </CautareDosare>
+    </soap12:Body>
+  </soap12:Envelope>
+  """.format(dataStart = dateStart, dataStop = dateEnd)
+  # print(qbody)
+  # breakpoint()
+  return qbody
+
+def api2xml(target, body):
+  response = requests.post(url,data=body,headers=headers)
+  ztring = response.content.decode("utf-8")
+  
+  with open(dir_responsexml + target + '_dosare.xml', "w") as text_file:
+      text_file.write(ztring)
+  print('- saved: ' + dir_responsexml + target + '_dosare.xml')
+
+  with gzip.open(dir_responsexmlgz + target + '_dosare.xml.gz', 'wt') as f:
+    f.write(ztring)
+  print('- saved: ' + dir_responsexmlgz + target + '_dosare.xml.gz')
+
+# TODO: this should happen in external script
+
+dates = ['2021-05-28', '2021-05-29', '2021-05-30']
+
+for oneday in dates:
+  filename=oneday
+  print('>> fetching Dosare date: ' + oneday + ' ...')
+  api2xml(oneday, cautaDosarXMLQ(oneday))
+  print('-- fetched Dosare date: ' + oneday)
--- readme.md
+++ readme.md
@@ -8,13 +8,30 @@ instanta = 'all' | [slug_instanta1, slug_instanta2, ...]
 
 if instanta='all' or None, loop all instanțe then fetch dosare & ședințe filtered by dateRange
 
+## Commands
+- `createDb.py <pato/to/db>` – creates sqlite db from config, if no path given
+- `fetch.py <date> <days=1> <direction=back>` : fetches date + days in direction [back|fwd] – fetches 24h after date
+- `updatedb.py` : looks into xml folder, writes to db, moves to /parsed folder; also checks for dupes? – if dupe create log
+
+## Roadmap
+
 ### v0.1
-- fetch api -> local xml
-- local xml -> sqlite
+- [x] fetch api ⟶ local xml
+- [x] local xml ⟶ sqlite prototype
 
 ### v0.2
-- cron
-- logging
-- fetch api -> update sqlite
-- check dupes?
+- [ ] add relationships
+- [ ] prototype fetch ⟶ db sequence for 24h (1day)
 
+### v0.3 
+- [ ] commands w arguments
+- [ ] logging
+- [ ] fetch api ⟶ update sqlite
+
+### v0.4
+- [ ] check for fetch errors
+- [ ] check dupes?
+- [ ] cron
+
+### v0.5
+- [ ] nosql db
