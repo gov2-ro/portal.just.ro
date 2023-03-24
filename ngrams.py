e8f7dc6b9fea91b17a6c174cdfa38ad32266b35c
pax <pax@mioritics.ro>
2023-03-24 13:00:00 +0200

    nicer ngrams

--- ngrams.py
+++ ngrams.py
@@ -1,13 +1,13 @@
-import csv, time, sqlite3
-from collections import Counter
-
 db_file = 'data/portal-just.db'
+sqlq = "SELECT soluţieSumar FROM DosarSedinta"
 output_csv = 'data/ngrams.csv'
 ignore_list = 'data/ignore_list.csv'
 min_words = 5
 max_words = 15
 no_results = 50
 
+import csv, time, sqlite3
+from collections import Counter
 
 def get_ngrams(text, min_words, max_words, ignore=None):
     words = text.split()
@@ -26,34 +26,30 @@ def get_most_common_ngrams(rows, k, ignore=None):
     ngram_counts = Counter(ngrams)
     return ngram_counts.most_common(k)
 
-# Read the ignore list from a CSV file
+start_time = time.time()
+print('-> Looking for most frequent [' + str(no_results) + '] ngrams between [' + str(min_words) + '] and [' + str(max_words) + '] words, ignoring the sets found in [' + ignore_list + ']')
+print('-> db: [' + db_file + ']')
+print('-> SQL: [' + sqlq + ']')
+
 ignore = set()
 with open(ignore_list, newline='') as f:
     reader = csv.reader(f)
     for row in reader:
         ignore.add(row[0])
 
-# Connect to the SQLite database
 conn = sqlite3.connect(db_file)
-
-# Get a cursor object
 cur = conn.cursor()
-
-# Query the database to get the rows to analyze
-cur.execute("SELECT soluţieSumar FROM DosarSedinta")
+cur.execute(sqlq)
 rows = cur.fetchall()
 
-# Get the 25 most common n-grams, excluding any n-grams in the ignore list
-start_time = time.time()
 most_common_ngrams = get_most_common_ngrams(rows, k=no_results, ignore=ignore)
 end_time = time.time()
 
-# Export the results to a CSV file
 with open(output_csv, 'w', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(['ngram', 'count'])
     for ngram, count in most_common_ngrams:
         writer.writerow([ngram, count])
 
-# Display the total execution time of the script
+print(">> DONE \n\r" + 'saved to ' + output_csv)
 print(f"Execution time: {end_time - start_time:.2f} seconds")
