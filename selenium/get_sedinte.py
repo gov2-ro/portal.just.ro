80643c3c53c8594b0cbe772799937b4006dfaab1
pax <pax@mioritics.ro>
2023-04-10 16:54:30 +0300

    to db

--- selenium/get_sedinte.py
+++ selenium/get_sedinte.py
@@ -1,4 +1,7 @@
-import time, sqlite3, csv
+import time, sqlite3, csv, os, sys
+from urllib.parse import urlparse, parse_qs
+from datetime import datetime
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -7,6 +10,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+
+dbfile = '../data/just-scraping.db'
+zitable = 'lista-sedinte'
 
 """ 
 for a list of instanțe,
@@ -25,6 +31,7 @@ def get_date(driver, url, date):
     zpath = "//div[@id='MSO_ContentTable']//div[@id='WebPartWPQ2']//table//td[contains(@class,'ms-dtinput')]//input[contains(@class,'ms-input')]"
     data = []
     next_page = 1 #read data table at least once.
+    next_img_element = None
     # Wait for up to 10 seconds for the element to be present in the DOM
     wait = WebDriverWait(driver, 10)
 
@@ -43,7 +50,7 @@ def get_date(driver, url, date):
     
     while next_page == 1:
          # TODO: wait untill element arrives
-        time.sleep(2.3)
+        time.sleep(.75)
         qdata_table = "//div[@id='WebPartWPQ3']//table[1]"
         qnext_table = "//div[@id='WebPartWPQ3']//table[2]"
 
@@ -92,18 +99,20 @@ def get_date(driver, url, date):
                 # links = cols[0].find_elementss_by_xpath('.//a')
                 links = cols[0].find_elements(By.XPATH, './/a')
                 if links:
-                    row_data['departament_href'] = links[0].get_attribute('href')
+                    parsed_url = urlparse(links[0].get_attribute('href'))
+                    query_params = parse_qs(parsed_url.query)
+                    row_data['idx'] = query_params['id_sedinta'][0]
                 # links = cols[1].find_elementss_by_xpath('.//a')
-                links = cols[1].find_elements(By.XPATH, './/a')
-                if links:
-                    row_data['complet_href'] = links[0].get_attribute('href')
+                # links = cols[1].find_elements(By.XPATH, './/a')
+                # if links:
+                #     row_data['complet_href'] = links[0].get_attribute('href')
                 data.append(row_data)
-        
-        if next_img_element:
+
+        if next_img_element is not None:
             last_a_element.click()
             print('clicked next')
             # TODO: wait untill element arrives
-            time.sleep(2.3)
+            time.sleep(1.1)
         else:
             next_page = 0  
     
@@ -111,15 +120,44 @@ def get_date(driver, url, date):
     return data
 
 
+if not os.access(dbfile, os.F_OK | os.W_OK):
+    print("Database file does not exist or is not writable: " + dbfile)
+    sys.exit(1) # Exit the script with error code 1
+
 driver = setup_method()
-tdata = get_date(driver, "https://portal.just.ro/3/SitePages/Lista_Sedinte.aspx?id_inst=3", "08.03.2023")
+
+id_instanta = 179
+zidate = "17.03.2021"
+
+# tdata = get_date(driver, "https://portal.just.ro/3/SitePages/Lista_Sedinte.aspx?id_inst=3", "08.03.2023")
+tdata = get_date(driver, "https://portal.just.ro/" + str(id_instanta) + "/SitePages/Lista_Sedinte.aspx?id_inst=179", zidate)
 
 
-# TODO: click next page
+
+conn = sqlite3.connect(dbfile)
+c = conn.cursor()
+c.execute('CREATE TABLE IF NOT EXISTS "' + zitable + '" (institutie int, zi text, departament text, complet text, ora text, idx text)')
+
+# for row in tdata:
+#     c.execute("INSERT INTO ' + zitable + ' VALUES (:departament, :complet, :ora, :idx)", row)
+    
+for row in tdata:
+    date_obj = datetime.strptime(zidate, '%d.%m.%Y')
+    c.execute("INSERT INTO \"" + zitable + "\" (departament, complet, ora, idx, institutie, zi) VALUES (:departament, :complet, :ora, :idx, :institutie, :zi)", {
+        'departament': row['departament'],
+        'complet': row['complet'],
+        'ora': row['ora'],
+        'idx': row['idx'],
+        'institutie': id_instanta,  
+        'zi': date_obj.date().isoformat()  
+    })
+
+
+conn.commit()
+conn.close()
 
 breakpoint()
 teardown_method(driver)
 
 
-# link = ntable.find_element(By.TAG_NAME, "a")
-# link.get_attribute('innerHTML')
+ 
